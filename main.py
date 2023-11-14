b94dae8fd57d4ad03a430b8e62323d8072fd46eb
chavan_akshay <chavanakshay29@gmail.com>
2023-11-14 16:51:53 +0100

    Evaluation Module

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from text_summarizer.pipeline.stage01_data_ingestion import DataIngestionTrainin
 from text_summarizer.pipeline.stage02_data_validation import DataValidationTrainingPipeline
 from text_summarizer.pipeline.stage03_data_transformation import DataTransformationTrainingPipeline
 from text_summarizer.pipeline.stage04_model_trainer import ModelTrainerPipeline
+from text_summarizer.pipeline.stage05_model_evaluation import ModelEvaluationPipeline
 from text_summarizer.logging import logger
 
 
@@ -39,14 +40,25 @@ from text_summarizer.logging import logger
 #     logger.exception(e)
 #     raise e
 
-STAGE_NAME = "Model Trainer Stage"
+# STAGE_NAME = "Model Trainer Stage"
+#
+# try:
+#     logger.info(f">>>>> Stage: {STAGE_NAME} started <<<<<")
+#     model_trainer = ModelTrainerPipeline()
+#     model_trainer.main()
+#     logger.info(f">>>>> Stage: {STAGE_NAME} completed <<<<<\n\nx===============x")
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
 
+
+STAGE_NAME = "Model Evaluation Stage"
 try:
+    logger.info(f"*******************************")
     logger.info(f">>>>> Stage: {STAGE_NAME} started <<<<<")
-    model_trainer = ModelTrainerPipeline()
-    model_trainer.main()
+    model_evaluation = ModelEvaluationPipeline()
+    model_evaluation.main()
     logger.info(f">>>>> Stage: {STAGE_NAME} completed <<<<<\n\nx===============x")
 except Exception as e:
     logger.exception(e)
     raise e
-
